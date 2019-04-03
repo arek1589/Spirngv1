211cfc2b1fd20de982c7520cd72ca79a0396ffaa
arek1589 <33283726+arek1589@users.noreply.github.com>
2019-04-03 13:03:18 +0200

    Update KursspringApplication.java

--- kursspring/src/main/java/com/clockworkjava/kursspring/KursspringApplication.java
+++ kursspring/src/main/java/com/clockworkjava/kursspring/KursspringApplication.java
@@ -14,6 +14,8 @@ public class KursspringApplication {
 		        SpringApplication.run(KursspringApplication.class, args);
 		        System.out.println("Arek i Patryk");
 		        System.out.println("Factory beauty");
+		        System.out.println("hejjj");
+
 
 	} 
 }   
